0a78eb68ceada7b92795525d59320a1a1b172e2e
Priyanka Mittal <priyankazapbuild@gmail.com>
2022-10-19 13:08:54 +0530

    integrate add posts api

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -6,6 +6,7 @@ const Blog = () => {
     const [openModal, setOpenModal] = useState(false);
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
+    const [image, setImage] = useState("");
     const [postData, setPostData] = useState();
     const handleAddPost = () => {
         setOpenModal(true);
@@ -15,45 +16,52 @@ const Blog = () => {
     }
 
     const submitPost = async () => {
-        console.log(apiUrl.api, "kkk");
         const value1 = title;
+        console.log("value1---",value1)
         const value2 = description;
-        console.log(value1, value2, "lll");
-        const res = await fetch(`${apiUrl.api}/post`, {
-            body: JSON.stringify({
-                title: value1,
-                description: value2
-            }),
+        const value3 = image;
+        console.log(value1, value2, value3, "lll");
+         await fetch('http://localhost:3000/api/addPost', {
             headers: {
                 'Content-Type': 'application/json',
+                Accept: 'application/json',
             },
-            method: 'POST',
-
-        });
-
-        const result = await res.json();
-        console.log(result, "result");
-        alert(`Is this your full name: ${result.name}`);
+            method: 'post',
+            body:{
+                title: value1,
+                description: value2,
+                image: value3
+            },
+           
+        })
+            .then((response) => {
+                console.log(response, "iii");
+                return response.json(); // do something with response JSON
+            });
     };
 
-    useEffect(async () => {
+    const fetchData = async () => {
+        // You can await here
         await fetch(`${apiUrl.api}/post`, {
             headers: {
                 'Content-Type': 'application/json',
             },
             method: 'GET',
-
         })
-            .then((result) => {
-                return result;
-            });
-        // .then(response => response.json())
+            // ...
+            // .then((result) => {
+            //     console.log(result, "response")
+            //     return result;
+            // })
+
+            .then(response => response.json())
         // .then(data => setPostData(data))
-    }, []
-    );
+    }
+    useEffect(() => {
+        fetchData();
+    }, []);
 
     return (
-        console.log(postData, "postData"),
         <><div className="container-fluid">
             <div className="main row">
                 <div className="col-md-10 col-9"><h1>Blog Posts</h1></div>
@@ -81,7 +89,7 @@ const Blog = () => {
                                     <button onClick={handleClosePost} type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div className="modal-body">
-                                    <form onSubmit={submitPost()}>
+                                    <form onSubmit={submitPost}>
                                         <div className="row">
                                             <div className="col-md-4 label">
                                                 <label>
@@ -120,15 +128,17 @@ const Blog = () => {
                                             <div className="col-md-8">
                                                 <input
                                                     type="file"
+                                                    value={image}
+                                                    onChange={e => setImage(e.target.value)}
                                                 />
                                             </div>
                                         </div>
-                                        {/* <input type="submit" value="Submit" /> */}
+                                        <input type="submit" className="btn btn-primary mt-4" />
                                     </form>
                                 </div>
                                 <div className="modal-footer">
                                     {/* <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button> */}
-                                    <button type="button" className="btn btn-primary">Save changes</button>
+                                    {/* <button type="button" className="btn btn-primary">Save changes</button> */}
                                 </div>
                             </div>
                         </div>
